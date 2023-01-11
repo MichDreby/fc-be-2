7a21feb88574f774b6b1dcebc8e46cfd0b66f9ea
michdreby <mr.sincee@gmail.com>
2023-01-11 13:02:54 +0300

    feat(setup): added security setup + common

---
+++ src/common/constants.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = 'FC REST API';
+export const APP_DESCRIPTION = 'FC REST API';
+export const APP_VERSION = '1.0.0';
+export const PASSWORD_HASH_SALT = 10;
+export const JWT_EXPIRES_IN = '7d';

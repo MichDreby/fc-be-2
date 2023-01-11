119f02a1f7e52f2ab71c85acec23c4e2ca299be3
michdreby <mr.sincee@gmail.com>
2023-01-11 12:22:29 +0300

    feat(database): added database module with configuration setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { NewsModule } from './news/news.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(4000),
+        NODE_ENV: Joi.string().default('development'),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        DB_SSL: Joi.boolean().required(),
+      }),
+    }),
+    DatabaseModule,
+    NewsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
